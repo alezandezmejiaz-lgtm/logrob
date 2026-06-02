f66b93035c1e9857758de2d5d9cde813a03ca7de
Luis Delgado <alezandezmejiaz@gmail.com>
2026-06-02 09:26:46 -0500

    nuebo

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstudianteController;
 
-Route::get('/estudiante',[EstudianteController::class,'getEstudiantes']);
-Route::get('/estudiante/{id}',[EstudianteController::class,'getEstudiante']);
-Route::post('/estudiante',[EstudianteController::class,'createEstudiante']);
-Route::put('/estudiante/{id}',[EstudianteController::class,'updateEstudiante']);
-Route::delete('/estudiante/{id}',[EstudianteController::class,'deleteEstudiante']);
+Route::get('/estudiante',[EstudianteController::class,'getEstudiantes']); //metodo para mostrar todo
+Route::get('/estudiante/{id}',[EstudianteController::class,'getEstudiante']);//metodo para buscar
+Route::post('/estudiante',[EstudianteController::class,'createEstudiante']); //metodo post para crear
+Route::put('/estudiante/{id}',[EstudianteController::class,'updateEstudiante']);//put para actualizar
+Route::delete('/estudiante/{id}',[EstudianteController::class,'deleteEstudiante']);//eliminar
